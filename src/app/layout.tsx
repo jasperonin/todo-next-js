69cf6eeb73fbfd5e62a31afa7c1b85fb61ff8fd5
Leo <snzleo@gmail.com>
2023-07-07 00:04:43 +0800

    created basic CRUD operation using state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
